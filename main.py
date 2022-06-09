f03d89b0dd7bbf92e19eed2624c61841aa8917a5
nastyacolfer <nastyakolfer@gmail.com>
2022-06-10 01:52:30 +0300

    categories

--- main.py
+++ main.py
@@ -5,28 +5,6 @@ import random
 token = '5520079906:AAG633_q2e2C9UCe3fPTVnRUAjuHPr7c4JA'
 bot = telebot.TeleBot(token)
 
-#
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-#     keyboard.row('Привет', 'Пока')
-#     bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
-#
-# @bot.message_handler(commands=['test'])
-# def start_message(message):
-#     markup = telebot.types.InlineKeyboardMarkup()
-#     markup.add(telebot.types.InlineKeyboardButton(text='Три', callback_data=3))
-#     markup.add(telebot.types.InlineKeyboardButton(text='Четыре', callback_data=4))
-#     markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
-#     bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
-#
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     if message.text.lower() == 'привет':
-#         bot.send_message(message.chat.id, 'Ещё раз привет!')
-#     elif message.text.lower() == 'пока':
-#         bot.send_message(message.chat.id, 'Пока!')
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
